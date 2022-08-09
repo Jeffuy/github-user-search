e1bfec827450531d4127cd9503ae22a6fa76d428
Jeff Free <jefffreelance3@gmail.com>
2022-08-09 15:51:19 -0300

    :sparkles: searcher added to the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Container } from "@mui/material";
+import Searcher from "./components/searcher/index";
 
 const App = () => {
-	return <div>Soy yo</div>;
+	const containerStyle = {
+		background: "whitesmoke",
+		width: "80vw",
+		height: "500px",
+		borderRadius: "16px",
+		marginTop: "40px",
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+	};
+
+	return (
+		<Container sx={containerStyle}>
+			<Searcher />
+		</Container>
+	);
 };
 
 export default App;
