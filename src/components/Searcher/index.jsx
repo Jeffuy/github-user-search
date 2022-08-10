9100787473174acd38ffe6df7f5eab878bc7e3df
Jeff Free <jefffreelance3@gmail.com>
2022-08-10 10:07:03 -0300

    :lipstick: searchbar styles added

--- src/components/Searcher/index.jsx
+++ src/components/Searcher/index.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import { IconButton, Stack, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Searcher = () => {
+	const stackStyles = {
+		marginTop: "30px",
+		width: "80%",
+	};
+
+	const textFieldStyles = {
+		width: "90%",
+	};
+
+	const iconButtonStyles = {
+		left: "-45px",
+	};
+
 	return (
-		<Stack>
-			<TextField label="Search">
-				<IconButton />
-			</TextField>
+		<Stack direction="row" sx={stackStyles}>
+			<TextField
+				sx={textFieldStyles}
+				id="outlined-basic"
+				label="GitHub User"
+				placeholder="Octocat"
+				variant="outlined"
+				size="small"
+			/>
+			<IconButton size="small" sx={iconButtonStyles}>
+				<SearchIcon />
+			</IconButton>
 		</Stack>
 	);
 };
